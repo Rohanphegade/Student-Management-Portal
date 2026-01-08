d0a68ef88e64b9dd8a8fb7c32627dde8883f7a47
Deepali Shrivastav <106448402+Deepali-Shrivastav@users.noreply.github.com>
2026-01-08 12:47:29 +0530

    Changed Database configuration for Postgresql

--- core/settings.py
+++ core/settings.py
@@ -80,10 +80,15 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'student_portal_db',
+        'USER': 'student_admin',
+        'PASSWORD': 'Studentportal',
+        'HOST': 'student-portal-db.ch8a4a2ga8cs.ap-south-1.rds.amazonaws.com',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
